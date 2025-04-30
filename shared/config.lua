5b28995d7274d2c72c510599e91af53b3e3ed10b
MaDHouSe <madhouse1979@gmail.com>
2025-04-30 11:33:37 +0200

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -3,7 +3,7 @@ Config = {}
 Config.OpenMenuButton = 'F4'  -- Keybind to open uber menu
 Config.OpenMenuCommand = 'ubermenu'  -- Command to open uber menu
 
-Config.RitPrice = 5 -- default 5
+Config.RitPrice = 5 -- default 5 per mil
 
 -- use it as table like this `{'muscle', 'super'}` (you can add moreif you want)
 -- or set to `-1` so that every vehicle class is allowed.
